2e75a78fef1e2da51a573c77623822c73457598a
Beatrice-cyber <93314184+Beatrice-cyber@users.noreply.github.com>
2023-02-26 14:01:40 +0000

    a model

--- Main.js
+++ Main.js
@@ -11,7 +11,7 @@ class AI{
         // output layer
         model.add(tf.layers.dense({units: 2}));
         model.compile({
-            loss: 'meanSquareError',
+            loss: 'meanSquaredError',
             optimizer:'sgd',//stat gradient descent
         });
         return model;
@@ -20,17 +20,22 @@ class AI{
     run(){
         const model = this.compile();
         // xs is the input layer 
-        const xs = tf.tensor2d([0.1,0.2,0.5], [1, 0.4,0.7]);
+        const xs = tf.tensor2d([
+            [0.1, 0.2, 0.5], 
+            [0.9, 0.4, 0.7],
+            [0.6 ,0.4, 0.7]]);
         // ys is the output layer
-        const ys = tf.tensor2d([1, 0],[ 0, 1], [1, 10]);
+        const ys = tf.tensor2d([[1, 0],[ 0, 1], [0.1, 1] ]);
         // train the model
         model.fit(xs, ys, {
             // traning for how many times 
             epochs: 100,
             // batchSize: 10,
             // shuffle: true,
-        }).then((result) => {
-            const data = tf.tensor2d([0.1,1.0,1.5])
+        }).then(() => {
+            const data = tf.tensor2d([
+                [0.1,1.0,1.5]
+            ])
             const prediction = model.predict(data);
             prediction.print();
         });
