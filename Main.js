c34d096b2c0722a35ae8a199039827bc3381ecbd
Beatrice-cyber <93314184+Beatrice-cyber@users.noreply.github.com>
2023-02-26 12:44:07 +0000

    a template for ML

--- Main.js
+++ Main.js
@@ -1,1 +1,37 @@
-console.log('hey i am an example');
+const tf = require('@tensorflow/tfjs');
+
+class AI{
+    // for compile the actual model , before using it 
+    compile (){
+        const model = tf.sequential();
+        // input layer 
+        model.add(tf.layers.dense({units: 3, inputShape: [3]}));
+        // hidden layer
+        // model.add(tf.layers.dense({units: 10, activation:'relu'}));
+        // output layer
+        model.add(tf.layers.dense({units: 2}));
+        model.compile({
+            loss: 'meanSquareError',
+            optimizer:'sgd',//stat gradient descent
+        });
+        return model;
+    }
+    // run the model 
+    run(){
+        const model = this.compile();
+        // xs is the input layer 
+        const xs = tf.tensor2d([0.1,0.2,0.5], [1, 0.4,0.7]);
+        // ys is the output layer
+        const ys = tf.tensor2d([1, 0],[ 0, 1], [1, 10]);
+        // train the model
+        model.fit(xs, ys, {
+            // traning for how many times 
+            epochs: 100,
+            batchSize: 10,
+            shuffle: true,
+        });
+    }
+}
+
+const ai = new AI();
+ai.run();
